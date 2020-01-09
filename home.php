401eb172d5d183181261a73dffca4d8e9fbd17b5
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2020-01-09 11:42:38 +0300

    Tenth commit

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
         <div class="post-content">
             <?php the_content(); ?>
         </div>
-        <a href="<?php the_permalink(); ?>">Read more</a>
+        <a href="<?php the_permalink(  )?>" class="btn btn-success">Read more</a>
     </article>
 <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
