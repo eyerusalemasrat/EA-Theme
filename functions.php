99751d3ac8bfd2f26829eec94f340b5161602ce1
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2020-01-16 22:59:52 +0300

    sidebar change

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ add_action( 'after_setup_theme', 'ea_theme_content_width', 0 );
 
 function new_excerpt_more($more) {
 	global $post;
-   return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Read the full article...</a>';
+   return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Read More...</a>';
   }
   add_filter('excerpt_more', 'new_excerpt_more');
 
