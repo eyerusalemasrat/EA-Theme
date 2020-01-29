a8e67308dab197ec169a9b89cef9900ece85e629
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2020-01-29 22:39:53 +0300

    some changes

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * EA-Theme functions and definitions
+ * EA functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package EA-Theme
+ * @package EA
  */
 
 if ( ! function_exists( 'ea_theme_setup' ) ) :
@@ -19,10 +19,10 @@ if ( ! function_exists( 'ea_theme_setup' ) ) :
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on EA-Theme, use a find and replace
-		 * to change 'ea-theme' to the name of your theme in all the template files.
+		 * If you're building a theme based on EA, use a find and replace
+		 * to change 'ea' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'ea-theme', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'ea', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -44,7 +44,7 @@ if ( ! function_exists( 'ea_theme_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'primary' => esc_html__( 'Primary', 'ea-theme' ),
+			'primary' => esc_html__( 'Primary', 'ea' ),
 		) );
 
 		/*
@@ -115,26 +115,26 @@ function new_excerpt_more($more) {
  * Enqueue scripts and styles.
  */
 function ea_theme_scripts() {
-	wp_enqueue_style( 'ea-theme_bootstrap_css', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'ea_bootstrap_css', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 
-	wp_enqueue_style( 'ea-theme_fontawesome', get_template_directory_uri() . '/assets/font/fonts/css/font-awesome.min.css' );
+	wp_enqueue_style( 'ea_fontawesome', get_template_directory_uri() . '/assets/font/fonts/css/font-awesome.min.css' );
 	
 
-	wp_enqueue_style( 'ea-theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'ea-style', get_stylesheet_uri() );
 
 	wp_register_script('popper', get_template_directory_uri() . '/assets/js/popper.min.js', array(), '20170710', true );
 
-	wp_enqueue_script( 'ea-theme-tether', get_template_directory_uri() . '/assets/js/tether.min.js', array(), '20170115', true );
+	wp_enqueue_script( 'ea-tether', get_template_directory_uri() . '/assets/js/tether.min.js', array(), '20170115', true );
 	 
-	wp_enqueue_script( 'ea-theme_bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '20170915', true );
+	wp_enqueue_script( 'ea_bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '20170915', true );
 
 
-	wp_enqueue_script( 'ea-theme-bootstrap-hover', get_template_directory_uri() . '/assets/js/bootstrap-hover.js', array('jquery'), '20170115', true ); 
+	wp_enqueue_script( 'ea-bootstrap-hover', get_template_directory_uri() . '/assets/js/bootstrap-hover.js', array('jquery'), '20170115', true ); 
 
-	wp_enqueue_script( 'ea-theme-nav-scroll', get_template_directory_uri() . '/assets/js/nav-scroll.js', array('jquery'), '20170115', true );
+	wp_enqueue_script( 'ea-nav-scroll', get_template_directory_uri() . '/assets/js/nav-scroll.js', array('jquery'), '20170115', true );
 
 
-	wp_enqueue_script( 'ea-theme-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'ea-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package EA-Theme
+ * @package EA
  */
 
 ?>
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site container">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ea-theme' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ea' ); ?></a>
 	
 	<header id="masthead" class="site-header row">
 	<nav id="menu" class="navbar navbar-expand-md navbar-light"  role="navigation" >
